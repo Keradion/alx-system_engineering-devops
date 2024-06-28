9911e012ff25f5be746cf9e3b0824ed64a230f43
Daniel Berihun <Danielshitaye10@gmail.com>
2024-06-28 05:45:53 -0700

    flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
 #!/usr/bin/pup
 # This puppet manifest Install flask(2.1.0) using pip3
 
-package { 'flask':
+package {'werkzeug':
+  ensure => '2.1.1',
+  provider => 'pip3'
+}
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
