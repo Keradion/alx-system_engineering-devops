34a1dc1382b710fc2352fd7552bf37f70757b74f
Daniel Berihun <danielshitaye10@gmail.com>
2024-07-30 04:52:13 -0700

    task0" && git push

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -17,11 +17,24 @@ if __name__ == '__main__':
 
     todo_response = requests.get(api_todo_url).json()
     user_response = requests.get(api_user_url)
+    employee_name = user_response.json()['name']
     user_name = user_response.json()['username']
 
     for each in todo_response:
+        if each.get('userId') == employee_id and each.get('completed') is True:
+            count = count + 1
+
+    print('Employee {} is done with tasks({}/20):'.format(
+        employee_name, count))
+
+    for each in todo_response:
+        if each.get('userId') == employee_id and each.get('completed') is True:
+            print('\t ', end='')
+            print(each['title'])
+
         if each.get('userId') == employee_id:
             with open('USER_ID.csv', 'a', newline='') as file:
                 writer_object = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
                 writer_object.writerow([each.get('userId'), user_name, each.get(
                     'completed'), each.get('title')])
+
