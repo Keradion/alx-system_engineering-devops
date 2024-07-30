a11c28d0a054569d7c0634747f00e8139632ca79
Daniel Berihun <danielshitaye10@gmail.com>
2024-07-30 02:55:57 -0700

    api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This script uses REST API and a given employee ID  to return information
+about the TODO list progress of the employee.
+"""
+import requests
+import sys
+
+employee_id = int(sys.argv[1])
+count = 0
+
+api_todo_url = 'https://jsonplaceholder.typicode.com/todos'
+api_user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(
+        employee_id)
+
+todo_response = requests.get(api_todo_url).json()
+user_response = requests.get(api_user_url)
+employee_name = user_response.json()['name']
+
+for each in todo_response:
+    if each['userId'] == employee_id and if each['completed'] is True:
+        count = count + 1
+
+print('Employee {} is done with tasks({}/20):'.format(employee_name, count))
+
+for each in todo_response:
+    if each['userId'] == employee_id and if each['completed'] is True:
+        print('     ', end='')
+        print(each['title'])
