fd3adeb013e7b284c00ca9f4b3c0d06982d0891a
Daniel Berihun <danielshitaye10@gmail.com>
2024-07-31 09:27:03 -0700

    export all employee data to json

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This script uses REST API and a given employee ID  to return information
+about the TODO list progress of the employee & exports the data into json.
+"""
+import csv
+import json
+import requests
+import sys
+
+if __name__ == '__main__':
+    temp_dict = {}
+    employee_dict = {}
+    employee_list = []
+
+    api_user_url = 'https://jsonplaceholder.typicode.com/users/'
+    user_response = requests.get(api_user_url).json()
+
+    api_todo_url = 'https://jsonplaceholder.typicode.com/todos/'
+    todo_response = requests.get(api_todo_url).json()
+
+
+    for user in user_response:
+        user_id = user['id']
+        employee_list = []
+        for todo in todo_response:
+            if user_id == todo['userId']:
+                temp_dict["username"] = user['username']
+                temp_dict['title'] = todo.get('title')
+                temp_dict['completed'] = todo.get('completed')
+                employee_list.append(temp_dict)
+                temp_dict = {}
+
+        employee_dict[user_id] = (employee_list)
+
+    with open('todo_all_employees.json', 'a') as json_file:
+        json_object = json.dumps(employee_dict)
+        json_file.write(json_object)
